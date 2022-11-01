b88047ee0822083be741082d18330c2082d99e42
Runt <qingingrunt2010@qq.com>
2022-11-01 23:12:32 +0800

    media_play

--- app/src/main/java/com/demo/open/MainActivity.java
+++ app/src/main/java/com/demo/open/MainActivity.java
@@ -2,13 +2,24 @@ package com.demo.open;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean isPause;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.image).setOnClickListener(v -> {
+            ((ImageView)v).setImageResource(!isPause ?R.drawable.media_pause_animate:R.drawable.media_play_animate);
+            AnimatedVectorDrawable drawable = (AnimatedVectorDrawable) ((ImageView)v).getDrawable();
+            drawable.start();
+            isPause = !isPause;
+
+        });
     }
 }
